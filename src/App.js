95d4a8678b207ee828d464887f749fb0313065f1
scattter <413365742@qq.com>
2021-03-13 00:54:31 +0800

    support Enzyme & Jest test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+// import './App.css';
+
+const handleClick = () => {
+  alert('LOVE')
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Demo</h1>
+      <h2 onClick={handleClick}>click will alert LOVE</h2>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import {configure, shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+configure({ adapter: new Adapter() })
+
+
+describe('App test', () => {
+  it('snapshot', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
