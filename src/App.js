b7a05d2bb1b64b5d46d12929cde215d80db8b40a
scattter <413365742@qq.com>
2021-03-13 04:03:36 +0800

    support scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import './App.scss'
+
 const handleClick = () => {
   alert('LOVE')
 }
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1 class='flex justify-center text-5xl my-4 font-serif'>Demo</h1>
       <h2 onClick={handleClick} class='flex justify-center font-mono my-2'>click will alert LOVE</h2>
+      <h3 className="app">222</h3>
     </div>
   );
 }
