d7a82c565f915776962c61448cb367bde7828e22
scattter <413365742@qq.com>
2021-03-13 17:39:12 +0800

    use className replace calss

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div>
       <LoginPage />
-      <h1 class='flex justify-center text-5xl my-4 font-serif'>Demo</h1>
+      <h1 className='flex justify-center text-5xl my-4 font-serif'>Demo</h1>
       <h2 onClick={handleClick} className='flex justify-center font-mono my-2'>click will alert LOVE</h2>
       <h3 className="app">222</h3>
     </div>
