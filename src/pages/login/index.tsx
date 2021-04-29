48c80d95952c5caa53de9a2f9cd4ea6be2d02f8d
scattter <413365742@qq.com>
2021-04-29 16:49:00 +0800

    add register page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,25 +3,38 @@ import { Component } from 'react'
 import { History } from 'history'
 
 import { Button } from 'antd'
-import { LockOutlined, UserOutlined } from '@ant-design/icons'
+import { LockOutlined, UserOutlined, PhoneOutlined } from '@ant-design/icons'
 
 interface Props {
 	history: History
 }
 
-interface State { }
+interface State {
+	isRegister: boolean
+}
 
 class LoginPage extends Component<Props, State> {
-	onClick = () => {
-		const { history } = this.props
-		history.push('/welcome')
+	state = {
+		isRegister: false
+	}
+
+	handleRegister = () => {
+		const { isRegister } = this.state
+		this.setState({
+			isRegister: !isRegister
+		})
 	}
 
 	render() {
+
+		const { isRegister } = this.state
+		const height = isRegister ? 'h-88' : 'h-80'
+
+
 		return (
 			<div className="flex bg-login bg-cover bg-top-4 h-screen">
-				<div className="mx-auto mt-32 rounded-lg w-60 h-80 bg-white shadow-2xl">
-					<h2 className="text-center font-serif text-2xl mt-7">LOGIN</h2>
+				<div className={"mx-auto mt-32 rounded-lg w-60 bg-white shadow-2xl " + height} >
+					<h2 className="text-center font-serif text-2xl mt-7">{isRegister ? 'REGISTER' : 'LOGIN'}</h2>
 					<div className="flex justify-center flex-col login-container">
 						<div className="mx-auto mt-10">
 							<UserOutlined className="m-2" />
@@ -31,6 +44,14 @@ class LoginPage extends Component<Props, State> {
 								placeholder="Enter your username"
 							/>
 						</div>
+						{isRegister && <div className="mx-auto mt-5">
+							<PhoneOutlined className="mx-2 my-1" />
+							<input
+								type="password"
+								className="w-3/4 mx-auto outline-none border-b-2"
+								placeholder="Enter your phone id"
+							/>
+						</div>}
 						<div className="mx-auto mt-5">
 							<LockOutlined className="mx-2 my-1" />
 							<input
@@ -42,11 +63,17 @@ class LoginPage extends Component<Props, State> {
 					</div>
 					<Button
 						className="flex justify-center focus:outline-none rounded-lg mx-auto mt-12 w-1/2 bg-blue-200"
-					>Sign In
+					>
+						{isRegister ? 'Sign Up' : 'Sign In'}
 					</Button>
-					<span className="flex justify-center cursor-pointer text-xs mt-5 text-blue-500">Not ready? Click here to register</span>
+					<span
+						className="switch-page-state flex justify-center cursor-pointer text-xs mt-5 text-blue-500"
+						onClick={this.handleRegister}
+					>
+						{isRegister ? 'Ready? Click here to login' : 'Not ready? Click here to register'}
+					</span>
 				</div>
-			</div>
+			</div >
 		)
 	}
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,14 @@ module.exports = {
     extend: {
       backgroundImage: theme => ({
         'login': "url('src/pages/login/img/login.jpg')"
-      })
+      }),
+      height: {
+        '88': '23rem'
+      },
+      backgroundPosition: {
+        'top-4': 'center top -4rem',
+      }
     },
-    backgroundPosition: {
-      bottom: 'bottom',
-      center: 'center',
-      left: 'left',
-      'left-bottom': 'left bottom',
-      'left-top': 'left top',
-      right: 'right',
-      'right-bottom': 'right bottom',
-      'right-top': 'right top',
-      top: 'top',
-      'top-4': 'center top -4rem',
-    }
   },
   variants: {
     extend: {},
